018472bf6d63a0c3342058c241037aa08e5310ba
随风 <2338244917@qq.com>
2024-03-03 16:15:43 +0800

    feature: 添加全局异常处理和全局成功处理，使得后面的接口设置更加合理

--- cloud_platform.go
+++ cloud_platform.go
@@ -1,21 +1,25 @@
 package main
 
 import (
-	"cloud-platform-system/internal/asynctask"
-	"cloud-platform-system/internal/config"
-	"cloud-platform-system/internal/handler"
-	"cloud-platform-system/internal/svc"
 	"context"
 	"flag"
 	"fmt"
-	"github.com/zeromicro/go-zero/core/logx"
-	"github.com/zeromicro/go-zero/core/service"
 	"log"
 	"net/http"
 	_ "net/http/pprof" // 目的: 让pprof包执行init函数将请求处理函数注册到default请求处理器中
 
+	"cloud-platform-system/internal/asynctask"
+	"cloud-platform-system/internal/common/errorx"
+	"cloud-platform-system/internal/config"
+	"cloud-platform-system/internal/handler"
+	"cloud-platform-system/internal/svc"
+	"cloud-platform-system/internal/types"
+
 	"github.com/zeromicro/go-zero/core/conf"
+	"github.com/zeromicro/go-zero/core/logx"
+	"github.com/zeromicro/go-zero/core/service"
 	"github.com/zeromicro/go-zero/rest"
+	"github.com/zeromicro/go-zero/rest/httpx"
 )
 
 var configFile = flag.String("f", "etc/cloud_platform.yaml", "the config file")
@@ -45,6 +49,31 @@ func main() {
 
 	// 注册路由
 	handler.RegisterHandlers(server, srvCtx)
+
+	// 全局成功处理
+	httpx.SetOkHandler(func(_ context.Context, data any) any {
+		switch t := data.(type) {
+		case *types.CommonResponse:
+
+			return t
+		default:
+
+			// 后面写接口就可以爽一点了
+			return map[string]any{"code": 200, "msg": "成功", "data": t}
+		}
+	})
+
+	// 全局异常处理
+	httpx.SetErrorHandler(func(err error) (int, any) {
+		switch e := err.(type) {
+		case *errorx.CodeError:
+
+			return e.Code, map[string]any{"code": e.Code, "msg": e.Msg}
+		default:
+
+			return http.StatusInternalServerError, map[string]any{"code": http.StatusInternalServerError, "msg": err.Error()}
+		}
+	})
 
 	// 启动
 	fmt.Printf("Starting server at %s:%d...\n", c.Host, c.Port)
