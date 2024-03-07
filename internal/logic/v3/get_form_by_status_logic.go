67fb92eba04855b4685e4113b792870d7347165c
随风 <2338244917@qq.com>
2024-03-07 20:16:30 +0800

    bug: user_form返回全部数据

--- internal/logic/v3/get_form_by_status_logic.go
+++ internal/logic/v3/get_form_by_status_logic.go
@@ -1,6 +1,7 @@
 package v3
 
 import (
+	"cloud-platform-system/internal/common/errorx"
 	"cloud-platform-system/internal/models"
 	"context"
 	"github.com/pkg/errors"
@@ -34,30 +35,36 @@ func (l *GetFormByStatusLogic) GetFormByStatus(req *types.GetFormByStatusRequest
 		return &types.CommonResponse{Code: 400, Msg: "status值错误"}, nil
 	}
 
-	// 查询
-	filter := bson.D{{"admin_id", admin.Id}, {"status", req.Status}}
-	cur, err := l.svcCtx.MongoClient.Database(l.svcCtx.Config.Mongo.DbName).Collection(models.UserApplicationFormDocument).Find(l.ctx, filter, options.Find().SetProjection(bson.D{{"user_id", 1}, {"explanation", 1}}))
+	// 查询管理员旗下所有订单
+	filter := bson.D{{"admin_id", admin.Id}}
+	cur, err := l.svcCtx.MongoClient.Database(l.svcCtx.Config.Mongo.DbName).Collection(models.UserApplicationFormDocument).Find(l.ctx, filter, options.Find().SetProjection(bson.D{{"admin_id", 0}}))
 	if err != nil {
-		l.Logger.Error(errors.Wrap(err, "can not get application_form data err"))
-		return &types.CommonResponse{Code: 500, Msg: "系统异常"}, nil
+		l.Logger.Error(errors.Wrap(err, "can not get user_application_form data err"))
+		return nil, errorx.NewCodeError(500, "can not get user_application_form data err")
 	}
 	defer cur.Close(l.ctx)
+
+	// 单个字段也不可以直接使用基础类型来进行解析
+	type Tmp struct {
+		Id          string `bson:"_id"`
+		UserId      string `bson:"user_id"`
+		Explanation string `bson:"explanation"`
+		Status      uint   `bson:"status"`
+	}
+
+	var idToTmp = make(map[string]*Tmp)
 	var ids []string
-	var idToExplanation = make(map[string]string)
+
 	for cur.Next(l.ctx) {
-		// 单个字段也不可以直接使用基础类型来进行解析
-		type Tmp struct {
-			Id          string `bson:"user_id"`
-			Explanation string `bson:"explanation"`
-		}
 		tmp := new(Tmp)
 		if err = cur.Decode(tmp); err != nil {
 			l.Logger.Error(errors.Wrap(err, "decode mongo data error"))
-			return &types.CommonResponse{Code: 500, Msg: "系统异常"}, nil
+			return &types.CommonResponse{Code: 500, Msg: "decode mongo data error"}, nil
 		}
-		ids = append(ids, tmp.Id)
-		idToExplanation[tmp.Id] = tmp.Explanation
+		ids = append(ids, tmp.UserId)
+		idToTmp[tmp.UserId] = tmp
 	}
+
 	if len(ids) == 0 {
 		return &types.CommonResponse{Code: 200, Msg: "获取成功"}, nil
 	}
@@ -66,25 +73,31 @@ func (l *GetFormByStatusLogic) GetFormByStatus(req *types.GetFormByStatusRequest
 	filter = bson.D{{"_id", bson.M{"$in": ids}}}
 	cur, err = l.svcCtx.MongoClient.Database(l.svcCtx.Config.Mongo.DbName).Collection(models.UserDocument).Find(l.ctx, filter, options.Find().SetProjection(bson.D{{"password", 0}, {"auth", 0}}))
 	if err != nil {
-		l.Logger.Error(errors.Wrap(err, "can not get users data err"))
+		l.Logger.Error(errors.Wrap(err, "can not get userApplications data err"))
 		return &types.CommonResponse{Code: 500, Msg: "系统异常"}, nil
 	}
 	defer cur.Close(l.ctx)
-	type UserData struct {
-		Id          string `bson:"_id" json:"id,omitempty"`
-		Email       string `bson:"email" json:"email,omitempty"`
-		Name        string `bson:"name" json:"name,omitempty"`
-		Explanation string `bson:"explanation,omitempty" json:"explanation,omitempty"`
+
+	type UserApplicationData struct {
+		Id          string `json:"id,omitempty"`
+		Email       string `bson:"email" json:"email"`
+		Name        string `bson:"name" json:"name"`
+		Explanation string `json:"explanation"`
+		Status      uint   `json:"status"`
 	}
-	var users []*UserData
+
+	var userApplications []*UserApplicationData
 	for cur.Next(l.ctx) {
-		user := new(UserData)
-		if err = cur.Decode(user); err != nil {
+		userApplication := new(UserApplicationData)
+		if err = cur.Decode(userApplication); err != nil {
 			l.Logger.Error(errors.Wrap(err, "decode mongo data error"))
-			return &types.CommonResponse{Code: 500, Msg: "系统异常"}, nil
+			return &types.CommonResponse{Code: 500, Msg: "decode mongo data error"}, nil
 		}
-		user.Explanation = idToExplanation[user.Id]
-		users = append(users, user)
+		tmp := idToTmp[userApplication.Id]
+		userApplication.Id = tmp.Id
+		userApplication.Explanation = tmp.Explanation
+		userApplication.Status = tmp.Status
+		userApplications = append(userApplications, userApplication)
 	}
-	return &types.CommonResponse{Code: 200, Msg: "获取成功", Data: map[string]any{"users": users}}, nil
+	return &types.CommonResponse{Code: 200, Msg: "获取成功", Data: map[string]any{"visitor_applications": userApplications}}, nil
 }
