05fb36916e68a538f7049e1e9f5dba9392e4abb9
随风 <2338244917@qq.com>
2024-03-10 13:47:25 +0800

    fix: return db image with creator msg

--- internal/logic/v3/get_db_image_by_id_logic.go
+++ internal/logic/v3/get_db_image_by_id_logic.go
@@ -28,10 +28,11 @@ func NewGetDbImageByIdLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Ge
 }
 
 func (l *GetDbImageByIdLogic) GetDbImageById(req *types.GetDbImageByIdReq) (resp *types.CommonResponse, err error) {
+	// get db image
 	ret := l.svcCtx.MongoClient.Database(l.svcCtx.Config.Mongo.DbName).Collection(models.DbImageDocument).FindOne(l.ctx, bson.D{{"_id", req.Id}})
 	if err = ret.Err(); err == mongo.ErrNoDocuments {
 		return &types.CommonResponse{Code: 400, Msg: "id有误获取失败"}, nil
-	} else if err == nil {
+	} else if err != nil {
 		l.Logger.Error(err)
 		return nil, errorx.NewCodeError(500, "get data error")
 	}
@@ -42,5 +43,19 @@ func (l *GetDbImageByIdLogic) GetDbImageById(req *types.GetDbImageByIdReq) (resp
 		return nil, errorx.NewCodeError(500, "decode data error")
 	}
 
-	return &types.CommonResponse{Code: 200, Msg: "成功", Data: map[string]any{"db_image": db}}, nil
+	// get creator
+	ret = l.svcCtx.MongoClient.Database(l.svcCtx.Config.Mongo.DbName).Collection(models.UserDocument).FindOne(l.ctx, bson.D{{"_id", db.CreatorId}})
+	if err != nil {
+		l.Logger.Error(err)
+		return nil, errorx.NewCodeError(500, "get data error")
+	}
+
+	user := new(models.User)
+	if err = ret.Decode(user); err != nil {
+		l.Logger.Error(err)
+		return nil, errorx.NewCodeError(500, "decode data error")
+	}
+
+	// return data
+	return &types.CommonResponse{Code: 200, Msg: "成功", Data: map[string]any{"db_image": map[string]any{"db_image": db, "user_msg": map[string]any{"email": user.Email, "name": user.Email}}}}, nil
 }
