98e11bcffa594862f0590f32bbc7811c6aa7c061
随风 <2338244917@qq.com>
2024-03-10 15:49:07 +0800

    fix: 把user和admin路径搞错了

--- internal/handler/routes.go
+++ internal/handler/routes.go
@@ -71,11 +71,6 @@ func RegisterHandlers(server *rest.Server, serverCtx *svc.ServiceContext) {
 					Handler: v2.GetAdminMsgByIdHandler(serverCtx),
 				},
 				{
-					Method:  http.MethodGet,
-					Path:    "/db-application/list",
-					Handler: v2.GetDbApplicationByIdHandler(serverCtx),
-				},
-				{
 					Method:  http.MethodPut,
 					Path:    "/db-container/change/:db_id/:status",
 					Handler: v2.UpdateDbStatusHandler(serverCtx),
@@ -104,6 +99,11 @@ func RegisterHandlers(server *rest.Server, serverCtx *svc.ServiceContext) {
 					Method:  http.MethodGet,
 					Path:    "/linux_container/list",
 					Handler: v2.GetLinuxContainerByUserIdHandler(serverCtx),
+				},
+				{
+					Method:  http.MethodGet,
+					Path:    "/myself/db-application/list",
+					Handler: v2.GetDbApplicationByIdHandler(serverCtx),
 				},
 				{
 					Method:  http.MethodPost,
@@ -165,6 +165,11 @@ func RegisterHandlers(server *rest.Server, serverCtx *svc.ServiceContext) {
 					Handler: v3.ChangeDbApplicationStatusHandler(serverCtx),
 				},
 				{
+					Method:  http.MethodGet,
+					Path:    "/db-application/list",
+					Handler: v3.GetDbApplicationListHandler(serverCtx),
+				},
+				{
 					Method:  http.MethodDelete,
 					Path:    "/db-image/:id",
 					Handler: v3.DelDbImageByIdHandler(serverCtx),
@@ -198,11 +203,6 @@ func RegisterHandlers(server *rest.Server, serverCtx *svc.ServiceContext) {
 					Method:  http.MethodPost,
 					Path:    "/linux/pull",
 					Handler: v3.PullLinuxImageHandler(serverCtx),
-				},
-				{
-					Method:  http.MethodGet,
-					Path:    "/me/db-application/list",
-					Handler: v3.GetDbApplicationListHandler(serverCtx),
 				},
 				{
 					Method:  http.MethodGet,
